b2e7840f06b167144b3d5a4fbbbed8b132b8dda5
vankatadam <vankatadam@gmail.com>
2020-06-22 22:01:36 +0200

    First pages initialised

---
+++ pages/getting-started.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function GettingStarted() {
+  return (
+    <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      Getting Started!
+    </div>
+  );
+}
