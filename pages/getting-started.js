b3a653beac36135d111677bd290b87e06c336cae
vankatadam <vankatadam@gmail.com>
2020-06-26 21:38:42 +0200

    CodeSandbox Test Button

--- pages/getting-started.js
+++ pages/getting-started.js
@@ -1,5 +1,16 @@
 import NavBar from "../components/Navigation-bar";
 
 export default function GettingStarted() {
-  return <NavBar color="blue"></NavBar>;
+  return (
+    <div>
+      <NavBar color="blue"></NavBar>
+
+      <a href="https://codesandbox.io/s/stupefied-framework-nmmvz?fontsize=14&hidenavigation=1&theme=dark">
+        <img
+          alt="Edit nmmvz"
+          src="https://codesandbox.io/static/img/play-codesandbox.svg"
+        />
+      </a>
+    </div>
+  );
 }
